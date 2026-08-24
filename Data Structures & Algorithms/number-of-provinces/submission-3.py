307a1a3fb358f118e51a3d2371350126553ac863
Sky Huang <160084258+kshuang04@users.noreply.github.com>
2026-08-24 13:48:47 -0700

    Add: number-of-provinces - submission-3

---
+++ Data Structures & Algorithms/number-of-provinces/submission-3.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        num_provinces = 0
+        visited = set()
+        n = len(isConnected)
+
+        def dfs(city):
+            stack = [city]
+
+            while stack:
+                top = stack.pop()
+                visited.add(top)
+                for i in range(n):
+                    if isConnected[top][i] == 1 and i not in visited:
+                        stack.append(i)
+
+        for i in range(n):
+            if i not in visited:
+                dfs(i)
+                num_provinces += 1
+        
+        return num_provinces

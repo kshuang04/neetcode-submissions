08e83b07978634dc6804e4a3136bcc1495711522
Sky Huang <160084258+kshuang04@users.noreply.github.com>
2026-08-26 13:33:51 -0700

    Add: max-consecutive-ones-iii - submission-4

---
+++ Data Structures & Algorithms/max-consecutive-ones-iii/submission-4.py
@@ -0,0 +1,22 @@
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        flips = k
+        count = 0
+        max_count = 0
+        l = 0
+
+        for r in range(len(nums)):
+            if nums[r] == 0:
+                if flips:
+                    flips -= 1
+                else:
+                    while not flips and l <= r:
+                        if nums[l] == 0:
+                            flips += 1
+                        l += 1
+                        count -= 1
+                    flips -= 1
+            count += 1
+            max_count = max(max_count, count)
+        
+        return max_count

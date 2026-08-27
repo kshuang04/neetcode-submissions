a10022b9179cac1ddd0f8859bdcc64e907fb9892
Sky Huang <160084258+kshuang04@users.noreply.github.com>
2026-08-26 18:00:00 -0700

    Add: meeting-schedule-ii - submission-5

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-5.py
@@ -0,0 +1,20 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        if not intervals:
+            return 0
+            
+        hashmap = defaultdict(int)
+
+        for interval in intervals:
+            for i in range(interval.start, interval.end):
+                hashmap[i] += 1
+        
+        return max(hashmap.values())
